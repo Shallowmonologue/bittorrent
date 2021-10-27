a18fead7e6dacdc29023ad99622d678a87cb6ee8
Rushikesh Patil <patilrn20.comp@coep.ac.in>
2021-10-27 18:26:42 +0000

    Upload settings.py

---
+++ settings.py
@@ -0,0 +1,7 @@
+SETTINGS = {'peer_id': b'-MY2282-123456789000',
+            'port': '6881',
+            'timeout': 5,
+            'timeout_for_peer': 10,
+            'protocol_name': b'BitTorrent protocol',
+            'max_ans_size': 2048,
+            'numwant': 30}
