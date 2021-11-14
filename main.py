f406757c57fb2864a4182080d25c890395155459
Rushikesh Patil <patilrn20.comp@coep.ac.in>
2021-11-14 16:55:52 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-from TorrentMetainfo import TorrentMetainfo
-from TrackerAPI import get_peers_list_by_torrent_metainfo
+from CLI import CLI
+import argparse
+import os
+import sys
+
+
+def is_valid_torrent_file(filename):
+    """
+    Check if the given torrent file exists and is a torrent file.
+    :param filename: file path
+    :return: bool
+    """
+    try:
+        if not os.path.isfile(filename):
+            raise RuntimeError(f"Exception: \"{filename}\" doesn't exist.")
+        elif not filename.endswith(".torrent"):
+            raise RuntimeError(f"Exception: \"{filename}\" is not a valid torrent file.")
+    except RuntimeError as e:
+        return False
+    return True
+
+
+def main():
+    # Create the parser
+    my_parser = argparse.ArgumentParser(description='Torrent Client to download files using .torrent files.')
+    # Add the arguments
+    my_parser.add_argument(action='store', dest='path', help='the path to the .torrent file')
+    # Execute the parse_args() method
+    args = my_parser.parse_args()
+    input_path = args.path
+
+    if not is_valid_torrent_file(input_path):
+        print('The file specified does not exist or is not a .torrent file.')
+        sys.exit()
+
+    CLI(path=input_path).run()
+
 
 if __name__ == '__main__':
-    # torrent file
-    filename = "sintel.torrent"
-
-    # extract metainfo from the torrent file
-    metainfo = TorrentMetainfo(filename)
-    print(metainfo)
-
-    # get peers from the metainfo
-    # if we get the socket.timeout, then tracker is not responsive
-    peers = get_peers_list_by_torrent_metainfo(metainfo)
-    print("Total peers:", len(peers))
-    print(peers)
+    main()
